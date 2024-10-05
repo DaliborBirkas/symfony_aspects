cb14b73c1e96f39fbe40a1470e29b05f13d2b67b
DaliborBirkas <dbirkas3@gmail.com>
2024-10-05 16:42:55 +0200

    deny access

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin')]
-    public function aaaa(): Response
+    #[Route('/admin', name: 'admin', methods: [Request::METHOD_GET])]
+    public function admin(): Response
     {
+        $this->isGranted("ROLE_AA");
+        //$this->denyAccessUnlessGranted("ROLE_ADMIN");
         return $this->render('pages/admin/admin.html.twig', [
         ]);
     }
+
+
 }
